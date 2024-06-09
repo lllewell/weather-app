da4b0bf94a8ed6a0d5c20b75265a4442e4998f8f
Alien <lmllewellyn@gmail.com>
2024-06-08 22:27:52 -0400

    added elements to start building cards on event listener, removed card in HTML

--- script.js
+++ script.js
@@ -7,14 +7,32 @@ searchForm.addEventListener('submit', function (event){
 
     const q = queryInput.value.trim();
     
-    // Get a error 401 for my API key, generated a new one and still getting that error
-    fetch('https:/wwww.api.openweathermap.org/data/2.5/forecast?q=' + q + '&appid=9e50b50ffff8484602ef079e171d8347')
+
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + q + '&appid=9e50b50ffff8484602ef079e171d8347&units=imperial')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log('SEARCH');
         console.log(data);
+
+        for (let result of data.list) {
+            const columnEl = document.createElement('div');
+            const dateEl = document.createElement('div');
+            const iconEl = document.createElement('div');
+            const tempEl = document.createElement('div');
+            const windSpeedEl = document.createElement('div');
+            const humidityEl = document.createElement('div');
+  
+            columnEl.className = 'col-12';
+            dateEl.className = 'card p-3 m-3';
+            dateEl.textContent = result.dt_txt;
+            iconEl.textContent = result.weather.icon;
+
+            columnEl.appendChild(dateEl);
+            dateEl.appendChild(iconEl);
+            resultsList.appendChild(columnEl);
+        }
     })
     .catch(function (err) {
         console.log(err);
