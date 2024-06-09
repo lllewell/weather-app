0baa5fb15e1c6f18ddf64cfaa32caa5b4507eb44
Alien <lmllewellyn@gmail.com>
2024-06-08 22:57:49 -0400

    added elements to cards in JS

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ searchForm.addEventListener('submit', function (event){
     const q = queryInput.value.trim();
     
 
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + q + '&appid=9e50b50ffff8484602ef079e171d8347&units=imperial')
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + q + '&appid=9e50b50ffff8484602ef079e171d8347&units=imperial&cnt=5')
     .then(function (response) {
         return response.json();
     })
@@ -16,22 +16,30 @@ searchForm.addEventListener('submit', function (event){
         console.log('SEARCH');
         console.log(data);
 
-        for (let result of data.list) {
+        for (let list of data.list) {
             const columnEl = document.createElement('div');
-            const dateEl = document.createElement('div');
-            const iconEl = document.createElement('div');
-            const tempEl = document.createElement('div');
-            const windSpeedEl = document.createElement('div');
-            const humidityEl = document.createElement('div');
+            const cardEl = document.createElement('div');
+            const dateEl = document.createElement('h3');
+            const iconEl = document.createElement('img');
+            const tempEl = document.createElement('p');
+            const windSpeedEl = document.createElement('p');
+            const humidityEl = document.createElement('p');
   
-            columnEl.className = 'col-12';
-            dateEl.className = 'card p-3 m-3';
-            dateEl.textContent = result.dt_txt;
-            iconEl.textContent = result.weather.icon;
+            columnEl.className = 'col-6';
+            cardEl.className = 'card p-3 m-3';
+            dateEl.textContent = list.dt_txt;
+            iconEl.textContent = list.weather.icon;
+            tempEl.textContent = list.main.temp;
+            windSpeedEl.textContent = list.wind.speed;
+            humidityEl.textContent = list.main.humidity;
 
-            columnEl.appendChild(dateEl);
-            dateEl.appendChild(iconEl);
-            resultsList.appendChild(columnEl);
+            columnEl.appendChild(cardEl);
+            cardEl.appendChild(dateEl);
+            cardEl.appendChild(iconEl);
+            cardEl.appendChild(tempEl);
+            cardEl.appendChild(windSpeedEl);
+            cardEl.appendChild(humidityEl);
+            resultsList.appendChild(cardEl);
         }
     })
     .catch(function (err) {
