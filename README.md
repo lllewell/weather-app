118a7b55e6fd1b0347d6fb29c93fc60a6b4426d6
Alien <lmllewellyn@gmail.com>
2024-06-10 23:49:41 -0400

    added images folder, wrote README file, fixed iconEl in script JS, added CSS folder to reduce iconEl size

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# weather-app
+# Weather Dashboard
+
+## Description
+
+This app was made for travellers so that they can view the weather in multiple cities and plan accordingly.
+
+## Usage
+
+To use this app the user will type in a city name into the search bar, press submit and be able to view the cities current weather as well as a 5-day forecast. The information provided will include the date, an icon representation of the weather, temperature, wind speed, and humidity. Once a city is viewed it will be added to the search history and be viewable as a button.
+
+![weather-dashboard](./images/Weather-Dashboard.png)
--- script.js
+++ script.js
@@ -7,7 +7,6 @@ searchForm.addEventListener('submit', function (event) {
 
     const q = queryInput.value.trim();
 
-    // API is still only for 5 days at 3 hour intervals, need to figure out how to get the five day forecast
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + q + '&appid=b4b4f7d3e6c6628438377015d4c26f2e&units=imperial')
         .then(function (response) {
             return response.json();
@@ -16,42 +15,36 @@ searchForm.addEventListener('submit', function (event) {
             console.log('SEARCH');
             console.log(data);
 
-            // for (let list of data.list) {
             for (let i = 0; i < data.list.length; i = i + 8) {
                 const list = data.list[i];
                 const columnEl = document.createElement('div');
                 const cardEl = document.createElement('div');
                 const dateEl = document.createElement('h3');
-                const iconEl = document.createElement('img');
-                // const iconElurl = 'https://openweathermap.org/img/wn/10d@2x.png'
+                const imgEl = document.createElement('img');
+                const iconEl = list.weather[0].icon;
                 const tempEl = document.createElement('p');
                 const windSpeedEl = document.createElement('p');
                 const humidityEl = document.createElement('p');
-                // History aside and button aren't showing up on page, need to troubleshoot
-                const historyButton = document.createElement('button');
-                const historyAside = document.createElement('aside');
+            
 
-                columnEl.className = 'col-6';
-                cardEl.className = 'card p-3 m-3';
-                historyAside.className = 'col-4 p-2 m-2';
+                columnEl.className = 'col-4 justify-content-center';
+                cardEl.className = 'card p-3 m-3 col-4 align-items-center';
+                imgEl.className = 'imgEl';
                 dateEl.textContent = dayjs(list.dt_txt).format('MM/DD/YYYY');
-                iconEl = list.weather.icon;
+                imgEl.src = 'https://openweathermap.org/img/wn/' + iconEl + '@2x.png';
                 tempEl.textContent = ('Temp: ' + list.main.temp + '°F');
                 windSpeedEl.textContent = ('Wind Speed: ' + list.wind.speed + 'mph');
                 humidityEl.textContent = ('Humidity: ' + list.main.humidity + '%');
-                historyButton.textContent = q;
-                historyAside.textContent = historyButton;
 
                 columnEl.appendChild(cardEl);
-                columnEl.appendChild(historyAside);
-                cardEl.appendChild(dateEl);
+                columnEl.appendChild(imgEl);
+                cardEl.appendChild(dateEl)
                 // Icon not showing up for some reason
-                cardEl.appendChild(iconEl);
+                cardEl.appendChild(imgEl);
                 cardEl.appendChild(tempEl);
                 cardEl.appendChild(windSpeedEl);
                 cardEl.appendChild(humidityEl);
                 resultsList.appendChild(cardEl);
-                historyAside.appendChild(historyButton);
             }
         })
         .catch(function (err) {
